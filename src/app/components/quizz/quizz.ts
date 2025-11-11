c71c1ad59816252a565cf3552b984f3499cf1c23
miriaamaral <miriaamaralcustodio@gmail.com>
2025-11-11 15:23:21 -0300

    feat: adicionei a lógica do Quiz com as propriedades e a funcionalidade implementada '@if (finished)' já está funcionando no HTML

--- src/app/components/quizz/quizz.ts
+++ src/app/components/quizz/quizz.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.html',
   styleUrl: './quizz.css',
 })
-export class Quizz {
 
+export class Quizz {
+  title: string = " "; // criei uma propriedade title do tipo string inicializada com uma string vazia, depois adiciono onde ela será apontada, nesse caso no html, no h3{‌{title}‌}, ou seja, falando de um jeito mais claro, o valor da propriedade title será exibido ali no h3 do html. No final, o que fiz foi deixar o título dinâmico, podendo ser alterado via código typescript.
+
+  // no Typescript não é uma boa prática usar o tipo any (que significa qualquer coisa), o ideal é tipar as variáveis, ou seja, dizer qual o tipo de dado que aquela variável vai armazenar, como string, number, boolean, array, etc. Mas para fins didáticos, vou deixar como any mesmo.
+  questions: any
+  questionSelected: any
+
+  ansewers: string[] = [];
+  ansewerSelected: string = "";
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
 }
